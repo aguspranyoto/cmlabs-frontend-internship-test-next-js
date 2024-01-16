0334d8842007d6897392b18475371d6756dc8280
aguspranyoto <agusprnyt@gmail.com>
2024-01-16 13:17:39 +0700

    fix error deploy meal detail page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 export default function Home() {
   const router = useRouter();
 
   router.push("/category");
 
-  return <div>Redirecting to /category...</div>;
+  return <div>Redirecting to category...</div>;
 }
